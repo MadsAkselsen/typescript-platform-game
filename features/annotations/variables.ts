788547f95236948755e3c12d61e7d6b0a0c8ad95
Mads Akselsen <akselsenmads@gmail.com>
2021-02-22 18:29:58 +0100

    added example to 1. when declaring variable on one line and then initializing it on another

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -41,3 +41,23 @@ const logNumber: (i: number) => void = (i: number) => {
 const json = '{"x":10, "y": 20}';
 const coordinates: { x: number; y: number } = JSON.parse(json); // JSON.parse returns any, because the resulting javascript that it returns can be any type. We therefore have to define the type of coordinates
 console.log(coordinates.y); // {x: 10, y: 20}
+
+// 2. when we want a variable to have a type that can't be inferred
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
+
+// 3. variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
