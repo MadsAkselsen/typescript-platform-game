42317d7c7e8e78ba6902a7c2ec0e40b25a574a11
Mads Akselsen <akselsenmads@gmail.com>
2021-02-22 19:00:33 +0100

    added destructuring

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -13,3 +13,31 @@ function divide(a: number, b: number): number {
 const multiply = function (a: number, b: number): number {
   return a * b;
 };
+
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+
+const forecast = {
+  date: new Date(),
+  weather: 'sunny',
+  weather2: 'sun',
+};
+
+// when we want to desctructure the parameters. below we are destructuring forest into date and weather
+const logWeather = ({
+  date,
+  weather,
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(forecast.date);
+  console.log(forecast.weather);
+};
+
+logWeather(forecast);
