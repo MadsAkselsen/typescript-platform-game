71ea7869197666e85d509141337d880b9fdec4ca
Mads Akselsen <akselsenmads@gmail.com>
2021-02-26 20:53:42 +0100

    added examples to arrays.ts

--- features/annotations/arrays.ts
+++ features/annotations/arrays.ts
@@ -1,1 +1,6 @@
-const carMarkers: string[] = [];
+const carMakers: string[] = [];
+const dates = [new Date(), new Date()];
+
+let carsByMake: string[][];
+
+carsByMake = [['f150', 'f234'], ['corolla'], ['camoro']];
