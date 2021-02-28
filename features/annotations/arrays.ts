d864bfc21de5e7fc4e3f1fb8d92731c9fe02c57a
Mads Akselsen <akselsenmads@gmail.com>
2021-02-28 19:10:41 +0100

    added examples of how arrays TS is useful

--- features/annotations/arrays.ts
+++ features/annotations/arrays.ts
@@ -4,3 +4,7 @@ const dates = [new Date(), new Date()];
 let carsByMake: string[][];
 
 carsByMake = [['f150', 'f234'], ['corolla'], ['camoro']];
+
+// Help with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
