ecc82093384bec25b649d9e5f8e3af21e45711c9
Mads Akselsen <akselsenmads@gmail.com>
2021-02-28 19:14:17 +0100

    added map()

--- features/annotations/arrays.ts
+++ features/annotations/arrays.ts
@@ -8,3 +8,11 @@ carsByMake = [['f150', 'f234'], ['corolla'], ['camoro']];
 // Help with inference when extracting values
 const car = carMakers[0];
 const myCar = carMakers.pop();
+
+// Prevent incompatible values
+carMakers.push(100);
+
+// Help with 'map'
+carMakers.map((car: string): string => {
+  return car;
+});
