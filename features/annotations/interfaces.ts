e9ae7df21e7e581c9faf7947b070c2c1255f071e
Mads Akselsen <akselsenmads@gmail.com>
2021-03-04 20:44:12 +0100

    added interface explanation

--- features/annotations/interfaces.ts
+++ features/annotations/interfaces.ts
@@ -1,3 +1,8 @@
+// an interface in the parameter of a function is like a gatekeeper to the function.
+// This means that objects inserted into the function must fulfil the interface.
+// The objects are allowed to have properties that the interface dont have as long
+// as they also have the props in the interface
+
 interface Reportable {
   summary(): string;
 }
