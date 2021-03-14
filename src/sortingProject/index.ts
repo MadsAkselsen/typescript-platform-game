4ae8a04cacc0abb1c53b52e99a1e2446c2d17ccd
Mads Akselsen <akselsenmads@gmail.com>
2021-03-14 18:13:07 +0100

    added CharactersCollection

--- src/sortingProject/index.ts
+++ src/sortingProject/index.ts
@@ -1,9 +1,15 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+/* const numbersCollection = new NumbersCollection([10, 3, -5, 0, 4]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
-console.log(numbersCollection.data);
+console.log(numbersCollection.data); */
+
+const charactersCollection = new CharactersCollection('myString');
+const sorter = new Sorter(charactersCollection);
+sorter.sort();
+console.log(charactersCollection.data);
 
 export function sortingProject() {}
