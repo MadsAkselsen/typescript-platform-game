8d5c8195ed12661fffe089080f12e20b5dc10b80
Mads Akselsen <akselsenmads@gmail.com>
2021-03-14 11:07:37 +0100

    added typeguards

--- src/sortingProject/index.ts
+++ src/sortingProject/index.ts
@@ -1,16 +1,28 @@
 class Sorter {
-  constructor(public collection: number[]) {}
+  // number[] and string types restricts the shared methods on each of these types.
+  // But the use of 'instance of' ensures that typescript will give full access
+  // to all the properties and methods on both strings and arrays when used.
+  // 'instance of' is called a typeguard
+  constructor(public collection: number[] | string) {}
 
   sort(): void {
     const { length } = this.collection;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHandNumber = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHandNumber;
+        // All of this only works if collection is number[]
+        // If collection is an aray of numbers
+        if (this.collection instanceof Array) {
+          if (this.collection[j] > this.collection[j + 1]) {
+            const leftHandNumber = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = leftHandNumber;
+          }
         }
+
+        // Only going to work if collection is a string
+        // If colleciton is a string, do this logic instead:
+        // ~~logic to compare and swap characters
       }
     }
   }
