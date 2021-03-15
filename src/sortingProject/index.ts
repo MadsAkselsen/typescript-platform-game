5cb82a1d53f40074965af98ce45c94d344234e69
Mads Akselsen <akselsenmads@gmail.com>
2021-03-15 20:45:58 +0100

    added abstract classes

--- src/sortingProject/index.ts
+++ src/sortingProject/index.ts
@@ -3,15 +3,13 @@ import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkededList';
 
-/* const numbersCollection = new NumbersCollection([10, 3, -5, 0, 4]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
-console.log(numbersCollection.data); */
+const numbersCollection = new NumbersCollection([10, 3, -5, 0, 4]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
-/* const charactersCollection = new CharactersCollection('myString');
-const sorter = new Sorter(charactersCollection);
-sorter.sort();
-console.log(charactersCollection.data); */
+const charactersCollection = new CharactersCollection('myString');
+charactersCollection.sort();
+console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
 linkedList.add(500);
@@ -21,8 +19,7 @@ linkedList.add(-463);
 linkedList.add(45);
 linkedList.add(82);
 
-const sorter = new Sorter(linkedList);
-sorter.sort();
+linkedList.sort();
 linkedList.print();
 
 export function sortingProject() {}
